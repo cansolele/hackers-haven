26d15c2667bfb7677ac6a00108b5afd43c59b771
cansolele <androiddevelop@yandex.ru>
2023-01-11 18:24:16 +0300

    icon,logo, button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,14 +1,17 @@
 import style from "./Header.module.css";
 import { FaQuestion } from "react-icons/fa";
-import { useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { IconContext } from "react-icons";
 import { ImCancelCircle } from "react-icons/im";
 import { useState } from "react";
+
+import hh_logo from "./hh_logo.svg";
 
 const Header = () => {
   const [modalWindow, setModalWindow] = useState(false);
   const [modalDescription, setModalDescription] = useState("");
   const location = useLocation();
+  const navigate = useNavigate();
   const titles = {
     "/": "HACKER'S HAVEN",
     "/nmap": "NMAP",
@@ -35,6 +38,10 @@ const Header = () => {
 
   return (
     <div className={style.header}>
+      <button onClick={() => navigate("/")} className={style.mp_btn}>
+        <img src={hh_logo} alt="hh_logo" className={style.hh_logo} />
+      </button>
+
       <h1 className={style.tool_name}>{titles[location.pathname]}</h1>
       <button onClick={handleSubmit} className={style.button_help}>
         <FaQuestion className={style.button_help_icon} />
