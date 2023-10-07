cdaf2a0e2c511ab0c50cbd4aebe6c2aed4a30f52
cansolele <androiddevelop@yandex.ru>
2023-10-07 16:26:07 +0300

    Remove components, kev fix

--- client/src/components/Tools/report-creator/ReportCreator.js
+++ client/src/components/Tools/report-creator/ReportCreator.js
@@ -7,17 +7,29 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import style from "./ReportCreator.module.css";
 
 const ReportCreator = React.memo(({ currentLanguage }) => {
-  const [exploitTableKeys, setExploitTableKeys] = useState([]);
+  const [exploitTableKeys, setExploitTableKeys] = useState([0]); // Начальный компонент всегда будет с ключом 0
 
   const addExploitTable = useCallback(() => {
     setExploitTableKeys((prevKeys) => [...prevKeys, prevKeys.length]);
   }, []);
 
+  const handleRemoveExploitTable = (keyToRemove) => {
+    if (exploitTableKeys.length > 1) {
+      setExploitTableKeys((prevKeys) =>
+        prevKeys.filter((key) => key !== keyToRemove)
+      );
+    }
+  };
+
   return (
     <Box className={style.report_creator}>
-      <ExploitsTable currentLanguage={currentLanguage} />
-      {exploitTableKeys.map((key) => (
-        <ExploitsTable key={key} currentLanguage={currentLanguage} />
+      {exploitTableKeys.map((key, index) => (
+        <ExploitsTable
+          key={key}
+          currentLanguage={currentLanguage}
+          onRemove={() => handleRemoveExploitTable(key)}
+          isRemovable={index > 0}
+        />
       ))}
       <Box className={style.add_exploit_table_container}>
         <IconButton onClick={addExploitTable}>
